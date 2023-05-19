492bd85f508d1d7cc4df85d92546cb68234d8683
Mohamed Samy <mohamedsamy7155@gmai.com>
2023-05-19 15:49:48 +0300

    Refactor code, add authorization utility file

--- utils/authorization.js
+++ utils/authorization.js
@@ -1,24 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+const { User } = require('../models/user');
+
 const getAuthenticatedUser = async (req) => {
-  // Extract the JWT token from the Authorization header
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    throw new Error("Authorization header missing.");
-  }
+  try {
+
+    // Extract the JWT token from the Authorization header
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      throw new Error("Authorization header missing.");
+    }
 
   const token = authHeader.split(' ')[1];
-
-  try {
     // Verify the JWT token and extract the user ID
     const decodedToken = jwt.verify(token, process.env.secret);
+    console.log("decodedToken: ", decodedToken);
 
     if (!decodedToken) {
       throw new Error("Invalid token.");
     }
 
     const userId = decodedToken.id;
+    console.log("userId: ", userId);
 
     // Find the user in the database by ID
     const user = await User.findById(userId);
+    console.log("user: ", user);
 
     if (!user) {
       throw new Error("User not found.");
@@ -26,8 +33,9 @@ const getAuthenticatedUser = async (req) => {
 
     return user;
   } catch (error) {
+    console.error(error);
     throw new Error("Authentication failed.");
   }
-};
+}
 
 module.exports = { getAuthenticatedUser };
