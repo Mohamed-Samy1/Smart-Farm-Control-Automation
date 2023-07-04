ad51deb34dcd6a38af0f0d5d7b32b69b71167bc0
Mohamed-Samy1 <mohamedsamy7155@gmail.com>
2023-07-04 14:08:43 +0300

    Add Automatic Control

--- controllers/data.js
+++ controllers/data.js
@@ -3,7 +3,8 @@ const { Farm } = require("../models/farm");
 
 const { getAuthenticatedUser } = require("../utils/authorization");
 
-const moment = require("moment");
+const { initializeMQTT } = require("../services/mqtt");
+
 
 // Get all data
 exports.getAllData = async (req, res) => {
@@ -17,7 +18,10 @@ exports.getAllData = async (req, res) => {
     res.status(200).json(data);
   } catch (error) {
     console.error("Error fetching data:", error);
-    res.status(500).json({ error: "Could not get the data, please check if you passed the correct token" });
+    res.status(500).json({
+      error:
+        "Could not get the data, please check if you passed the correct token",
+    });
   }
 };
 
@@ -50,6 +54,75 @@ exports.getFarmData = async (req, res) => {
     res.status(200).json(data);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "An error occurred retreiving the farm data." });
+    res
+      .status(500)
+      .json({ message: "An error occurred retreiving the farm data." });
+  }
+};
+
+// Endpoint to handle manual control of devices
+exports.manualControl = async (req, res, next) => {
+
+  const mqtt = require("mqtt");
+
+  // MQTT Credentials and connection
+  const options = {
+    host: process.env.HOST,
+    port: process.env.MQTTPORT,
+    protocol: process.env.PROTOCOL,
+    username: "Samy1",
+    password: process.env.PASSWORD,
+  };
+
+  // initialize the MQTT client
+  var client = mqtt.connect(options);
+
+  // setup the callbacks
+  client.on("connect", function () {
+    console.log("Connected to HiveMQ");
+  });
+
+  client.on("error", function (error) {
+    console.error("Error connecting to HiveMQ Cloud:", error);
+  });
+
+  function subscribeToMainTopic(topic) {
+    // Subscribe to the topic
+    client.subscribe(topic, function (err) {
+      if (err) {
+        console.log("Error subscribing to topic:", err);
+      } else {
+        console.log("Subscribed to topic" + " " + topic);
+      }
+    });
+  }
+
+  const { serialNumber, device, state, isAuto } = req.body;
+
+  if (!isAuto) {
+    client.unsubscribe("hydra-main");
+    console.log('Unsubscribed to hydra-main, Manual Control is now On.');
+    
+    // Check if the device and state fields are present in the request body
+    if (!device || !state || !serialNumber) {
+      return res
+        .status(400)
+        .send("Serial Number, Device, and state fields are required");
+    }
+    // Publish a message to the relevant MQTT topic based on the device and state
+    const topic = `${device}/${serialNumber}/`;
+    const message = state === "on" ? "1" : "0";
+    client.publish(topic, message, (err) => {
+      if (err) {
+        console.error(`Error publishing message to topic ${topic}:`, err);
+        return res.status(500).send("Error publishing message to MQTT broker");
+      }
+      console.log(`Published message ${message} to topic ${topic}`);
+      return res.status(200).send(`Successfully turned ${state} ${device}`);
+    });
+  } else if (isAuto) {
+    initializeMQTT(isAuto);
+    console.log('Subscribed to hydra-main, Automatic Control is now On.');
+    next();
   }
 };
--- routes/data.js
+++ routes/data.js
@@ -8,4 +8,7 @@ router.get('/', dataController.getAllData);
 // Get all data of a specific farm
 router.post('/', dataController.getFarmData);
 
+// Handle manual control of devices
+router.post('/manualControl', dataController.manualControl);
+
 module.exports = router;
