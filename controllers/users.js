b8076e9631ac19648e1c828200fa419135298a6f
Mohamed-Samy1 <mohamedsamy7155@gmail.com>
2023-07-15 10:07:27 +0300

    Refactor code - login and registration

--- controllers/users.js
+++ controllers/users.js
@@ -3,21 +3,19 @@ const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 
 const { User } = require("../models/user");
-const { getAuthenticatedUser } = require('../utils/authorization');
+const { getAuthenticatedUser } = require("../utils/authorization");
 
 //The next line needs to be checked
 const { threadId } = require("worker_threads");
 
-const maxAge = 24 * 60 * 60;
-
 // Creating a JWT token
 const createJWT = (id) => {
   return jwt.sign(
-    { id }, 
+    { id },
     process.env.secret,
-    // token expiration, calculated by second
-    {expiresIn: maxAge * 1000}
-);
+    // token expiration, calculated by seconds (2 hours in this case)
+    { expiresIn: 7200 }
+  );
 };
 
 // ERROR HANDLING FUNCTION
@@ -46,33 +44,49 @@ const alertError = (err) => {
 
 exports.register = async (req, res) => {
   try {
-    const { 
-      firstName, 
-      lastName, 
-      email, 
-      password, 
-      confirmPassword, 
-      phone, 
-      country 
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+      phone,
+      country,
     } = req.body;
 
     if (password !== confirmPassword) {
-      return res.status(400).json({ error: 'Password and confirm password do not match' });
+      return res
+        .status(400)
+        .json({ error: "Password and confirm password do not match" });
     }
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ error: 'User with this email already exists' });
+      return res
+        .status(400)
+        .json({ error: "User with this email already exists" });
     }
 
-    const user = new User({ firstName, lastName, email, password, phone, country });
+    const user = new User({
+      firstName,
+      lastName,
+      email,
+      password,
+      phone,
+      country,
+    });
     const savedUser = await user.save();
 
     const token = createJWT(savedUser._id);
-    res.status(201).json({ token });
+    res
+      .status(201)
+      .json({
+        message: `${firstName} ${lastName} registered successfully.`,
+        token,
+      });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Registration failed.' });
+    res.status(500).json({ error: "Registration failed." });
   }
 };
 
@@ -82,18 +96,23 @@ exports.login = async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
 
     if (!user) {
-      return res.status(404).send("The user was not found!");
+      return res.status(404).json({ message: "User was not found!" });
     }
 
     if (user && bcrypt.compareSync(req.body.password, user.password)) {
       const token = createJWT(user._id);
-      res.status(200).json({ token });
+      res
+        .status(200)
+        .json({
+          message: `${firstName} ${lastName} logged in successfully.`,
+          token,
+        });
     } else {
-      return res.status(400).send("Wrong Password.");
+      return res.status(400).json({ message: "Wrong Password." });
     }
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Login failed.' });
+    res.status(500).json({ error: "Login failed." });
   }
 };
 
@@ -105,41 +124,40 @@ exports.logout = (req, res) => {
 
 //Getting the list of all users
 exports.getAllUsers = async (req, res) => {
-  try{
+  try {
     const user = await getAuthenticatedUser(req);
-  
+
     const userList = await User.find().select("-password");
 
     if (!userList) {
       return res.status(500).json({ success: false });
     }
     res.status(200).send(userList);
-  } catch(e) {
-    return res.status(401).json({ error: 'Authentication failed' });
+  } catch (e) {
+    return res.status(401).json({ error: "Authentication failed" });
   }
-  
 };
 
 //GETTING A SPECIFIC USER
 exports.getUser = async (req, res) => {
   try {
     // Extract the JWT token from the Authorization header
-    const token = req.headers.authorization.split(' ')[1];
+    const token = req.headers.authorization.split(" ")[1];
 
     // Verify the JWT token and extract the user ID
     const decodedToken = jwt.verify(token, process.env.secret);
     const userId = decodedToken.id;
 
     // Find the user in the database by ID and exclude the password field
-    const user = await User.findById(userId).select('-password');
+    const user = await User.findById(userId).select("-password");
 
     if (!user) {
-      throw new Error('User not found');
+      throw new Error("User not found");
     }
 
     res.status(200).json(user);
   } catch (e) {
-    res.status(401).json({ error: 'Authentication failed' });
+    res.status(401).json({ error: "Authentication failed" });
   }
 };
 
@@ -164,7 +182,7 @@ exports.updateUser = async (req, res) => {
         password: newPassword,
         phone: req.body.phone,
         country: req.body.country,
-        role: req.body.role
+        role: req.body.role,
       },
       { new: true }
     );
@@ -175,10 +193,9 @@ exports.updateUser = async (req, res) => {
 
     res.status(200).send(user);
   } catch (e) {
-    res.status(401).json({ error: 'Authentication failed' });
+    res.status(401).json({ error: "Authentication failed" });
   }
 };
-
 
 //DELETE AN EXISTING USER
 exports.deleteUser = async (req, res) => {
@@ -188,12 +205,12 @@ exports.deleteUser = async (req, res) => {
     const deletedUser = await User.findByIdAndRemove(user._id);
     if (deletedUser) {
       return res
-      .status(200)
-      .json({ success: true, message: "the user is deleted!" });
+        .status(200)
+        .json({ success: true, message: "the user is deleted!" });
     } else {
       return res
-      .status(404)
-      .json({ success: false, message: "user not found!" });
+        .status(404)
+        .json({ success: false, message: "user not found!" });
     }
   } catch (e) {
     return res.status(500).json({ success: false, error: e });
@@ -224,7 +241,6 @@ exports.postReset = async (req, res) => {
       });
   });
 };
-
 
 //GETTING A NEW PASSWORD
 exports.getNewPassword = async (req, res, next) => {};
