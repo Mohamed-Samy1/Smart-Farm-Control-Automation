28ad9005dd40ede930071cf2d20a152e3ec5656d
Mohamed-Samy1 <mohamedsamy7155@gmail.com>
2024-11-30 21:47:52 +0200

    Fix farm ownerships documentation

--- routes/farmOwnerships.js
+++ routes/farmOwnerships.js
@@ -80,72 +80,46 @@ router.put('/changeFarmOwner', farmsOwnershipsController.changeFarmOwner);
 /**
  * @swagger
  * /farms/ownershiphistory:
- *   put:
- *     summary: Change the ownership of a farm
+ *   get:
+ *     summary: Get the ownership history of farms
  *     tags:
  *       - FarmsOwnerships
- *     description: This endpoint allows changing the ownership of a farm by transferring it from the current owner to a new owner, based on the farm's serial number and the new owner's email.
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               serialNumber:
- *                 type: string
- *                 description: The serial number of the farm.
- *                 example: "12345"
- *               newOwnerEmail:
- *                 type: string
- *                 description: The email of the new owner of the farm.
- *                 example: "newowner@example.com"
+ *     description: This endpoint retrieves the history of farm ownerships, listing all transfers of ownership between users.
  *     responses:
- *       201:
- *         description: Successfully changed farm ownership.
+ *       200:
+ *         description: Successfully retrieved the farms ownership history.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   prevOwner_id:
+ *                     type: string
+ *                     description: The ID of the previous owner.
+ *                   prevOwner_name:
+ *                     type: string
+ *                     description: The full name of the previous owner.
+ *                   nextOwner_id:
+ *                     type: string
+ *                     description: The ID of the new owner.
+ *                   nextOwner_name:
+ *                     type: string
+ *                     description: The full name of the new owner.
+ *                   serialNumber:
+ *                     type: string
+ *                     description: The serial number of the farm.
+ *       500:
+ *         description: Failed to fetch farms ownership history due to an internal server error.
  *         content:
  *           application/json:
  *             schema:
  *               type: object
  *               properties:
- *                 farmsOwnerships:
- *                   type: object
- *                   properties:
- *                     prevOwner_id:
- *                       type: string
- *                       description: The ID of the previous owner.
- *                     prevOwner_name:
- *                       type: string
- *                       description: The full name of the previous owner.
- *                     nextOwner_id:
- *                       type: string
- *                       description: The ID of the new owner.
- *                     nextOwner_name:
- *                       type: string
- *                       description: The full name of the new owner.
- *                     serialNumber:
- *                       type: string
- *                       description: The serial number of the farm.
- *       400:
- *         description: Failed to change farm ownership due to an error.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
+ *                 error:
  *                   type: string
- *                   example: "Moving the farm ownership failed!"
- *       404:
- *         description: Farm or new owner not found.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "The farm was not found!" 
+ *                   example: "Failed to fetch farms ownership history."
  */
 //Get All Items
 router.get('/ownershiphistory', farmsOwnershipsController.getFarmsOwnershipsHistory);
